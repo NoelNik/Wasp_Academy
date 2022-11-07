1328e85202ed2f2c006aa179fa75825c0bc5eb60
NoelNik <leon0208042@gmail.com>
2022-11-08 00:58:27 +0300

    Made first three tasks

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lessons
 {
@@ -7,8 +8,73 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            Console.WriteLine("LOL");
         }
     }
 }
+
+
+/* First_task
+ * for (int i = 1000; i <= 9999; i++)
+            {
+                int first = i / 1000;
+                int second = i / 100 % 10;
+                int therd = i / 10 % 10;
+                int fourth = i % 10;
+                while ((first > second) && (second > therd) && (therd > fourth))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+ */
+
+/* Second_task
+ * Console.WriteLine("Enter the number >>>   ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j) { Console.Write($"{size, -4}"); }
+                    if (j > i) { Console.Write($"{size - j + i, -4}"); }
+                    if (j < i) { Console.Write($"{size - i + j, -4}"); }
+                }
+                Console.Write("\n\n");
+            }
+*/
+
+/* Third_task
+ * Console.WriteLine("Enter the cords >>> ");
+            int[,] cords = new int[3, 2];
+            for (int i = 0; i < 3; i++)
+            {
+                int[] first = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                cords[i, 0] = first[0];
+                cords[i, 1] = first[1];
+            }
+            int x_max = 0;
+            int y_max = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (cords[i, 0] > x_max) { x_max = cords[i, 0]; }
+                if (cords[i, 1] > y_max) { y_max = cords[i, 1]; }
+            }
+            for (int i = 0; i <= y_max; i++)
+            {
+                for (int j = 0; j <= x_max; j++)
+                {
+                    int first_check = ((cords[0, 0] - j) * (cords[1, 1] - cords[0, 1])) - ((cords[1, 0] - cords[0, 0]) * (cords[0, 1] - i));
+                    int second_check = ((cords[1, 0] - j) * (cords[2, 1] - cords[1, 1])) - ((cords[2, 0] - cords[1, 0]) * (cords[1, 1] - i));
+                    int third_check = ((cords[2, 0] - j) * (cords[0, 1] - cords[2, 1])) - ((cords[0, 0] - cords[2, 0]) * (cords[2, 1] - i));
+                    
+                    if ((first_check >= 0) && (second_check >= 0) && (third_check >= 0) ||
+                        (first_check <= 0) && (second_check <= 0) && (third_check <= 0))
+                    {
+                        Console.Write("* ");
+                    }
+                    else { Console.Write("_ "); }
+                }
+                Console.WriteLine("\n");
+            }
+*/
